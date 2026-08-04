7fcd2a6ea660e9baa581927fb3aecc396fd925cf
GTouchet <guillaumetouchet921@gmail.com>
2026-08-04 21:16:19 +0200

    Potential fix for tab icon

--- app/home.component.ts
+++ app/home.component.ts
@@ -11,7 +11,7 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
             Welcome to the MVLib Testing Ground
         </h3>
         <p>
-            This is a test application for the MvLib library. It is used to test its components and their functionalities.<br/>
+            This is a test application for the MVLib library. It is used to test its components and their functionalities.<br/>
             Choose a component from the header to see the examples and test the components styles, effects and settings.<br/>
             The project is still in its early stages, so you may encounter some bugs or missing features.<br/>
             I am actively working on fixing issues and adding new functionality.<br/>
